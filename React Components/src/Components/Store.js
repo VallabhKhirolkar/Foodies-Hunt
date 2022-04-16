d9e818b816bb39cdda42a8399c767ee2c63a385e
Mitesh-1998 <97754435+Mitesh-1998@users.noreply.github.com>
2022-04-16 09:21:13 +0530

    Old Components deleted from repo

--- React Components/src/Components/Store.js
+++
@@ -1,8 +0,0 @@
-import reducer from "./Reducer";
-import { createStore } from "redux";
-
-const IntialState = {loggedin : false};
-
-const mystore = createStore(reducer ,IntialState);
-
-export default mystore;
